bd93ff4601fa965c3869a452c5ad50fb61a222a6
Klevin Delimeta <klevindelimeta@hotmail.com>
2016-03-23 19:38:06 +0100

    //Test method read()

--- AdsAttacher/src/JUnit_Tests/TestXMLFileManipulation.java
+++ AdsAttacher/src/JUnit_Tests/TestXMLFileManipulation.java
@@ -1,12 +1,26 @@
 package JUnit_Tests;
 
-import static org.junit.Assert.*;
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
 
 import org.junit.Test;
+
+import model.UpdaterObject;
+import model.XMLFileManipulation;
 
 public class TestXMLFileManipulation {
 	@Test
 	public void test(){
 		//Test for xml file manipulation
+		XMLFileManipulation xmlFM = new XMLFileManipulation(new File("").getAbsolutePath()+"/updater.xml");
+		UpdaterObject updateObject = new UpdaterObject();
+		try {
+			updateObject = (UpdaterObject)xmlFM.Read(new UpdaterObject().getClass());
+		} catch (NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println(updateObject.getCurrentVersion(0)+"I did this shit");
 	}
 }
