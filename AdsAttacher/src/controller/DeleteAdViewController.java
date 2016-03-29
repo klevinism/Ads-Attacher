5dd376abf11f033fb96f5477e91cb937d8495a83
klevinism <klevindelimeta@hotmail.com>
2016-03-29 17:12:47 +0200

    //new objects created

---
+++ AdsAttacher/src/controller/DeleteAdViewController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import model.DeleteAdViewObject;
+import model.MainViewInputObject;
+
+public class DeleteAdViewController extends Controller {
+	private DeleteAdViewObject InputObject;
+	
+	public DeleteAdViewController(DeleteAdViewObject inputObject){
+		InputObject = inputObject;
+	}
+
+	@Override
+	public void setActionPerformed(String actionPerformer) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+	
+}
---
+++ AdsAttacher/src/model/PostObject.java
@@ -0,0 +1,52 @@
+package model;
+
+public class PostObject{
+	private String Id;
+	private String Title;
+	private String Author;
+	private String Category;
+	private String Date;	
+	
+	public PostObject(){
+		
+	}
+	
+	public PostObject(String title, String author, String category, String date){
+		Title = title;
+		Author = author;
+		Category = category;
+		Date = date;
+	}
+	
+	public void setTitle(String title){
+		Title = title;
+	}
+	
+	public String getTitle(){
+		return Title;
+	}
+	
+	public void setAuthor(String author){
+		Author = author;
+	}
+	
+	public String getAuthor(){
+		return Author;
+	}
+	
+	public void setCategory(String category){
+		Category = category;
+	}
+	
+	public String getCategory(){
+		return Category;
+	}
+
+	public void setDate(String date){
+		Date = date;
+	}
+	
+	public String getDate(){
+		return Date;
+	}
+}
